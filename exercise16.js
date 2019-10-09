0299060fcc4bdb34d6251ee11cab78a8ee96efa9
memorvin <alvina.tjia@yahoo.com>
2019-10-09 13:29:54 +0700

    exercise16

---
+++ exercise16.js
@@ -0,0 +1,133 @@
+//Logic Challenge: Graduates
+
+function graduates (students) {
+  var result = {};
+//return empty object if there's no input
+  if (students.length === 0) {
+    return result;
+  } else {
+//create array inside result object for each class
+    for (var i = 0; i < students.length; i++) {
+      if (result.hasOwnProperty(students[i]['class']) === false) {
+        result[students[i]['class']] = [];
+      }
+    }
+//create object for each graduates and push them to associated array of class inside result object
+    for (var key in result) {
+      for (var i = 0; i < students.length; i++) {
+        var objResult = {};
+        if (students[i].score > 75 && key === students[i].class) {
+          objResult.name = students[i].name;
+          objResult.score = students[i].score;
+          result[key].push(objResult); 
+        }
+      }
+    }
+    return result;
+  }
+}
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
